f1139f98efac9c8ea97a3578aabc2e7a1dc264d2
Kristerpedersen <kristerpedersen98@gmail.com>
2018-12-30 14:29:15 +0100

    main

--- Customer SErvice.py
+++ Customer SErvice.py
@@ -31,11 +31,11 @@ def home_visit(purpose="none"):
 def existing_customer():
     print("What kind of support do you need? \n [1] Television Support \n [2] Internet Support \n [3] Speak to a support representative.  ")
     response = input("What kind of support do you need?")
-    if response == 1:
+    if response == "1":
         television_support()
-    elif response == 2:
+    elif response == "2":
         internet_support()
-    elif response == 3:
+    elif response == "3":
         live_rep("support")
     else:
         print("Sorry, we didn't understand your selection. Please try again")
@@ -44,11 +44,11 @@ def existing_customer():
 def new_customer():
     print(" [1] Sign up for service. \n [2] Schedule a home visit. \n [3] Speak to a sales representative.")
     response = input("We're excited to have you join the DNS family, how can we help you?")
-    if response == 1:
+    if response == "1":
         sign_up()
-    elif response == 2:
+    elif response == "2":
         home_visit()
-    elif response == 3:
+    elif response == "3":
         live_rep("sales")
     else:
         print("Sorry, we didn't understand your selection.")
@@ -57,13 +57,13 @@ def new_customer():
 def sign_up():
     print("Great choice, friend! We're excited to have you join the DNS family!")
     response = input("Please select the package you are interested in signing up for. \n [1] Bundle Deal (Internet + Cable) \n [2] Internet \n [3] Cable")
-    if response == 1:
+    if response == "1":
         print("You've selected the Bundle Package! Please schedule a home visit and our technician will come and set up your new service.")
         home_visit("new install")
-    elif response == 2:
+    elif response == "2":
         print("You've selected the Internet Only Package! Please schedule a home visit and our technician will come and set up your new service.")
         home_visit("new install")
-    elif response == 3:
+    elif response == "3":
         print("You've selected the Cable Only Package! Please schedule a home visit and our technician will come and set up your new service.")
         home_visit("new install")
     else:
@@ -82,13 +82,13 @@ def live_rep(purpose):
 
 def did_that_help():
     didithelp = input("Did that help? \n [1] Yes \n [2] No")
-    if didithelp == 1:
+    if didithelp == "1":
         print("Thank you!")
-    if didithelp == 2:
+    if didithelp == "2":
         help = input("Would you like to talk to a live representative or schedule a home visit? \n [1] Live rep \n [2] Home visit")
-    elif help == 1:
+    elif help == "1":
             live_rep("support")
-    elif help == 2:
+    elif help == "2":
             home_visit("support")
     else:
         print("Sorry, we didn't understand your selection.")
@@ -98,16 +98,16 @@ def did_that_help():
 
 def television_support():
     response = input("What is the nature of your problem? \n [1] I can't access certain channels. \n [2] My picture is blurry. \n [3] I keep losing service.")
-    if response == 1:
+    if response == "1":
         print("Please check the channel lists at DefinitelyNotSinister.com. If the channel you cannot access is there, please contact a live representative.") 
         did_that_help()
-    elif response == 2:
+    elif response == "2":
         print("Try adjusting the antenna above your television set.")
         did_that_help()
-    elif response == 3:
+    elif response == "3":
         print("Is it raining outside? If so, wait until it is not raining and then try again.")
         did_that_help()
-    elif response == 4:
+    elif response == "4":
         live_rep("support")
     else:
         print(" Sorry, we didn't understand your selection.")
@@ -115,16 +115,16 @@ def television_support():
 
 def internet_support():
     response = input("What is the nature of your problem? \n [1] I can't connect to the internet. \n [2] My connection is very slow. \n [3] I can't access certain sites.")
-    if response == 1:
+    if response == "1":
         print("Unplug your router, then plug it back in, then give it a good whack, like the Fonz.")
         did_that_help()
-    elif response == 2:
+    elif response == "2":
         print("Make sure that all cell phones and other peoples computers are not connected to the internet, so that you can have all the bandwidth.")
         did_that_help()
-    elif response == 3:
+    elif response == "3":
         print("Move to a different region or install a VPN. Some areas block certain sites.")
         did_that_help()
-    elif response == 4:
+    elif response == "4":
         live_rep("support")
     else:
         print(" Sorry, we didn't understand your selection.")
@@ -140,9 +140,9 @@ def internet_support():
 def cs_service_bot():
     print("Hello! Welcome to the DNS Cable Company\'s Service Portal. Are you a new or existing customer? \n [1] New Customer \n [2] Existing Customer")
     response = input("Please enter the number corresponding to your choice: ")
-    if response == 1:
+    if response == "1":
         new_customer()
-    elif response == 2:
+    elif response == "2":
         existing_customer()
     else:
         print("Sorry, we didn't understand your selection. Please try again")
